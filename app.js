8a802be2b27c3a58116c647851af4254c737b658
Waleed-Rasheed <usamapagal75@gmail.com>
2024-02-22 07:52:01 +0500

    Work Done

---
+++ app.js
@@ -0,0 +1,61 @@
+var num1 = 8;
+var num2 = 4;
+
+var result = num1++ + num2++ + ++num2 + num2++ + num1++
+
+console.log(result)
+
+var num1 = 2;
+var num2 = 4;
+var num3 = 6;
+
+var result = --num1 + --num2 + num3++ + num3-- + num1
+
+console.log(result)
+
+
+var num1 = 3;
+var num2 = 6;
+var num3 = 9;
+
+var result = ++num1 + --num2 + num3-- + num3++ + num1
+
+console.log(result)
+
+
+var num1 = 4;
+var num2 = 8;
+var num3 = 12;
+
+var result = --num1 + ++num2 + num3-- + num3++ + num1
+
+console.log(result)
+
+
+var num1 = 6;
+var num2 = 4;
+var num3 = 8;
+
+var result = ++num1 + --num2 + ++num3 + num3-- + num1++
+
+console.log(result)
+
+
+var num1 = 3;
+var num2 = 4;
+var num3 = 9;
+var num4 = 8;
+
+var result = --num1 + --num2 + num3++ + num3-- + num4++ + --num4 
+
+console.log(result)
+
+
+var num1 = 3;
+var num2 = 6;
+var num3 = 9;
+var num4 = 12;
+
+var result = ++num1 + --num2 + num3++ + num3-- + num4++ + --num4 + num1
+
+console.log(result)
